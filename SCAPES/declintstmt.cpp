dbcde9b33c55907dd4dce3ae297d54549dab0316
Dicong Zhao <dicongzhao@cmail.carleton.ca>
2019-11-06 19:17:03 -0500

    Deleted test file

---
+++ SCAPES/declintstmt.cpp
@@ -0,0 +1,18 @@
+#include "declintstmt.h"
+
+DeclIntStmt::DeclIntStmt(string input)
+{
+
+}
+
+void DeclIntStmt::Compile()
+{
+
+
+}
+
+void DeclIntStmt::Run()
+{
+
+
+}
---
+++ SCAPES/declintstmt.h
@@ -0,0 +1,14 @@
+#ifndef DECLINTSTMT_H
+#define DECLINTSTMT_H
+#include <string>
+using namespace std;
+
+class DeclIntStmt
+{
+public:
+    DeclIntStmt();
+protected:
+    void Compile();
+    void Run();
+};
+#endif // DECLINTSTMT_H

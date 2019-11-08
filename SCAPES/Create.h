eebdd84d1fe2f87e1a6b26522cd93e8dbb1f137c
Marco <marcoshen@cmail.carleton.ca>
2019-11-08 17:23:30 -0500

    Created Create.cpp and Save.cpp

---
+++ SCAPES/Create.h
@@ -0,0 +1,16 @@
+#ifndef CREATE_H
+#define CREATE_H
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Create{
+
+public:
+  Create();
+  Create(string p);
+};
+
+#endif // CREATE_H
--- SCAPES/test.cpp
+++ SCAPES/test.cpp
@@ -1,6 +1,22 @@
 #include "test.h"
+#include <iostream>
 
-test::test()
-{
+#include "Create.h"
+#include "Save.h"
+#include <vector>
 
+using namespace std;
+
+int main(){
+
+
+  // create a program
+  vector<string> inst;
+  inst.push_back("test1");
+  inst.push_back("test2");
+  inst.push_back("test3");
+  inst.push_back("test4");
+  inst.push_back("test5");
+  Save{"Pgm1", inst, "../programs/"};
+  //Create createWithPath("../testPath/");
 }
